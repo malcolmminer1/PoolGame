7dbd4a774f0e6997ca534d8a1b9b9ecbbc0aadab
malcolmminer1 <malcolmminer1@gmail.com>
2025-05-20 14:41:46 -0500

    JS Changes

--- script.js
+++ script.js
@@ -1,36 +1,28 @@
 // MAIN CODE
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
-let cueball = makeBall(160, 250);
 
 let touchStartX = 0;
 let touchStartY = 0;
 let touchEndX = 0;
 let touchEndY = 0;
 
-// physics variables, but best practce is to create an object to hold these
-let xPos = 100;
-let yPos = 200;
-let size = 30;
-let xVel = 0;
-let yVel = 0;
-let xAcc = 0;
-let yAcc = 0.1;        // gravity
-let bounciness = 0.65;  // from 0 - 1
-
+let table = makeTable();
+let cueball = makeBall(160, 250, "#ffffdd");
 setupTouch();
 animate();
 
-function makeBall(x, y){
+function makeBall(x, y, colorIn){
   const ball = {
     radius: 10,
     xPos: x,
     yPos: y,
-    xVel: 0,
-    yVel: 0,
+    xVel: 10,
+    yVel: 5.5,
     xAcc: 0,
     yAcc: 0,
-    color: "#ff0000",
+    friction: 0.98,
+    color: colorIn,
     draw: function(){
       ctx.beginPath();
       ctx.arc(this.xPos, this.yPos, this.radius, 0, 2 * Math.PI);
@@ -38,8 +30,23 @@ function makeBall(x, y){
       ctx.fill();
     },
     update: function(){
+      if (table.pocketable(this.xPos, this.yPos)){
+        this.xAcc = 0;
+        this.yAcc = 0;
+        this.xVel = 0;
+        this.yVel = 0;
+        this.xPos = 0;
+        this.yPos = 0;
+        this.color = "#222222";
+      }
       this.xVel += this.xAcc;
       this.yVel += this.yAcc;
+      this.xVel *= this.friction;
+      this.yVel *= this.friction;
+      if (Math.hypot(this.xVel, this.yVel) < 0.1){
+        this.xVel = 0;
+        this.yVel = 0;
+      }
       this.xPos += this.xVel;
       this.yPos += this.yVel;
       // bounce off walls
@@ -68,54 +75,50 @@ function makeBall(x, y){
   return ball;
 }
 
+function makeTable(){
+  const table = 
+  { 
+    pocketRadius: 15,
+    pocketColor: "#222222",
+    pockets : [
+      {x : 4, y : 4},
+      {x : 4, y : canvas.height / 2},
+      {x : 4, y : canvas.height - 4},
+      {x : canvas.width - 4, y : 4},
+      {x : canvas.width - 4, y : canvas.height / 2},
+      {x : canvas.width - 4, y : canvas.height - 4}
+    ],
+    draw: function(){
+      ctx.fillStyle  = "#008000";
+      ctx.fillRect(0,0,canvas.width,canvas.height);
+      for (const loc of this.pockets){
+        ctx.beginPath();
+        ctx.arc(loc.x, loc.y, this.pocketRadius, 0, 2 * Math.PI);
+        ctx.fillStyle = this.pocketColor;
+        ctx.fill();
+      }
+    },
+    pocketable: function(ballX, ballY){
+      for (const loc of this.pockets){
+        if( Math.hypot(ballX - loc.x, ballY - loc.y) < this.pocketRadius){
+          console.log("Pocketed");
+          return true;
+        }
+      }
+      return false;
+    }
+  }
+  return table;
+}
 
 function animate(){
   // draw
-  ctx.clearRect(0,0,canvas.width,canvas.height);
+  table.draw();
   cueball.draw();
   // update
   cueball.update();
   // repeat
   window.requestAnimationFrame(animate);
-}
-
-function drawChar(x, y) {
-  // body
-  ctx.beginPath();
-  ctx.arc(x, y, 30, 0, 2 * Math.PI);
-  ctx.fillStyle = "#003388"
-  ctx.fill();
-  // left eye
-  ctx.beginPath();
-  ctx.arc(x - 15, y - 10, 12, 0, 2 * Math.PI);
-  ctx.fillStyle = "#ffffff"
-  ctx.fill();
-  // left pupil
-  ctx.beginPath();
-  ctx.arc(x - 15, y - 5, 6, 0, 2 * Math.PI);
-  ctx.fillStyle = "#000000"
-  ctx.fill();
-  // right eye
-  ctx.beginPath();
-  ctx.arc(x + 15, y - 10, 12, 0, 2 * Math.PI);
-  ctx.fillStyle = "#ffffff"
-  ctx.fill();
-  // right pupil
-  ctx.beginPath();
-  ctx.arc(x + 15, y - 5, 6, 0, 2 * Math.PI);
-  ctx.fillStyle = "#000000"
-  ctx.fill();
-  // nose
-  ctx.beginPath();
-  ctx.arc(x, y + 4, 5, 0, 2 * Math.PI);
-  ctx.fillStyle = "#FF8888"
-  ctx.fill();
-  // mouth
-  ctx.beginPath();
-  ctx.arc(x, y - 10, 30, 1.2, 2);
-  ctx.lineWidth = 2;
-  ctx.strokeStyle = "#FF8888";
-  ctx.stroke();
 }
 
 function setupTouch() {
@@ -135,7 +138,6 @@ function processTouch() {
   let deltaY = (touchEndY - touchStartY);
   cueball.push(deltaX, deltaY);
 }
-
 
 
 
